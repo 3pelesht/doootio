74bdbc436c4b20554b4365feebe8a1bdd31f893e
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2017-10-19 18:42:00 +0330

    fps button reaction to frame change

--- src/js/play.js
+++ src/js/play.js
@@ -2,19 +2,21 @@ var animator = null;
 
 function runAnimation()
 {
-	console.log(1);
 	_FRAMENUMBER++;
 	if (!maps[_FRAMENUMBER])
 	{
 		_FRAMENUMBER = 1;
 	}
+
 	$('.row').css({
 		'background-color': maps[_FRAMENUMBER].backgroundColor
 	});
 
 	mapDraw();
+
 	$('.frame').removeClass('active');
 	$('.frame').eq(_FRAMENUMBER - 1).addClass('active');
+	$('.icon-fps').attr('data-fps', maps[_FRAMENUMBER].fps);
 
 	// Execute function 'run' every ... second
 	var milliSeconds = maps[_FRAMENUMBER].fps * 1000;
@@ -23,7 +25,6 @@ function runAnimation()
 
 $('.play').click(function(e)
 {
-	console.log(2);
 	$(this).toggleClass('icon-play3 icon-pause2');
 	if ( $(this).hasClass('icon-play3') )
 	{
@@ -33,11 +34,9 @@ $('.play').click(function(e)
 	}
 	else
 	{
-		console.log(3);
 		// play
 		Animate_Is_Run = true;
 		var milliSeconds = maps[_FRAMENUMBER].fps * 1000;
-		console.log(milliSeconds);
 		// runAnimation();
 		animator = setTimeout('runAnimation()', milliSeconds);
 	}
