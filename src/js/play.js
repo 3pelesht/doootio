8ffb9fee4a25d4743ed25426776a191e2300e78a
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2017-10-19 18:39:12 +0330

    bind fps to animation

--- src/js/play.js
+++ src/js/play.js
@@ -1,34 +1,44 @@
 var animator = null;
 
+function runAnimation()
+{
+	console.log(1);
+	_FRAMENUMBER++;
+	if (!maps[_FRAMENUMBER])
+	{
+		_FRAMENUMBER = 1;
+	}
+	$('.row').css({
+		'background-color': maps[_FRAMENUMBER].backgroundColor
+	});
+
+	mapDraw();
+	$('.frame').removeClass('active');
+	$('.frame').eq(_FRAMENUMBER - 1).addClass('active');
+
+	// Execute function 'run' every ... second
+	var milliSeconds = maps[_FRAMENUMBER].fps * 1000;
+	animator = setTimeout('runAnimation()', milliSeconds);
+}
+
 $('.play').click(function(e)
 {
-	var milliSeconds = $('#second').val() * 1000;
-
+	console.log(2);
 	$(this).toggleClass('icon-play3 icon-pause2');
 	if ( $(this).hasClass('icon-play3') )
 	{
 		// pause
 		Animate_Is_Run = false;
-		clearInterval(animator);
+		clearTimeout(animator);
 	}
 	else
 	{
+		console.log(3);
 		// play
 		Animate_Is_Run = true;
-		animator = setInterval(function()
-		{
-			_FRAMENUMBER++;
-			if (!maps[_FRAMENUMBER])
-			{
-				_FRAMENUMBER = 1;
-			}
-			$('.row').css({
-				'background-color': maps[_FRAMENUMBER].backgroundColor
-			});
-
-			mapDraw();
-			$('.frame').removeClass('active');
-			$('.frame').eq(_FRAMENUMBER - 1).addClass('active');
-		}, milliSeconds);
+		var milliSeconds = maps[_FRAMENUMBER].fps * 1000;
+		console.log(milliSeconds);
+		// runAnimation();
+		animator = setTimeout('runAnimation()', milliSeconds);
 	}
 });
